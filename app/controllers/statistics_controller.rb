99ad718d1b05775eadcea3586f13bfd09882dde2
usui kazuki <usui-kazuki@outlook.jp>
2016-11-17 11:14:53 +0900

    月別円グラフ

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,10 +1,28 @@
 class StatisticsController < ApplicationController
   def circle_graph
-  	@sum_price = Price.where(loginuser: session[:usr]).where(hanbetsu: "支出").includes(:category).group(:categoryname).sum(:price)
-    @sum_price2 = Price.where(loginuser: session[:usr]).where(hanbetsu: "収入").includes(:category).group(:categoryname).sum(:price)
+  	if params[:m].present?
+  	  @month = Date.parse(params[:m])
+  	else
+  	  @month = Date.today
+    end
+	smonth = @month.beginning_of_month
+	emonth = @month.end_of_month
+  	logger.debug smonth
+  	logger.debug emonth
+
+  	@sum_price = Price.where(loginuser: session[:usr])
+  					.where(hanbetsu: "支出")
+  					.where(date: smonth..emonth)
+  					.includes(:category).group(:categoryname).sum(:price)
+
+    @sum_price2 = Price.where(loginuser: session[:usr])
+    				.where(hanbetsu: "収入")
+    				.where(date: smonth..emonth)
+    				.includes(:category).group(:categoryname).sum(:price)
 
   end
 
   def bar_graph
   end
 end
+ 
