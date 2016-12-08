3f2480f486edcb2877918b75db90587a572209ae
usui kazuki <usui-kazuki@outlook.jp>
2016-12-08 11:58:18 +0900

    目標をユーザー別に過去のお知らせを出さない

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,4 @@
 class Goal < ActiveRecord::Base
 	belongs_to :loginuser
-
-    validates_uniqueness_of :date, :message => "は既に登録されています"
+    validates_uniqueness_of :date, :message => "は既に登録されています" ,scope: [:loginuser]
 end
