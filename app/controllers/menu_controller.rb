b072f76f0817bf390ea82225bd7eb5041a568e2d
usui kazuki <usui-kazuki@outlook.jp>
2016-12-16 11:51:12 +0900

    目標残高の変更

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,6 +1,10 @@
 class MenuController < ApplicationController
   def index
   	@month = Date.today
+    @emonth = @month.end_of_month
+    @eday = @month.end_of_month.day
+    @day = Date.today.day
+    @zday = @eday - @day
 
     @usemoney = Price.where(loginuser: session[:usr])
     					.where(hanbetsu: "収入")
@@ -13,7 +17,7 @@ class MenuController < ApplicationController
     					.sum(:price)
 
     @tgoal = Goal.where(loginuser: session[:usr]).sum(:goalmoney)
-    
+    @zgoal = @tgoal - @usedmoney
     @notices = Notice.where(loginuser: session[:usr])
   end
 end
